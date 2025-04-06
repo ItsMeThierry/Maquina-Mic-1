6316f6d6eb21f9304193366b47337313afea53a7
Germano Lucas <germanolucasolr13@gmail.com>
2025-04-06 10:44:44 -0300

    Etapa 2 - Parte 2

---
+++ Maquina-Mic-1-main/src/file_manager.cpp
@@ -0,0 +1,112 @@
+#include "include/file_manager.h"
+#include "include/ula.h"
+#include <string>
+
+void FileManager::read_file(){
+    std::ifstream file("files/input.txt");
+
+    if(!file.is_open()){
+        std::cerr << "[Erro] Nao foi possivel abrir o input.txt" << std::endl;
+        return;
+    }
+
+    std::ofstream log("files/log.txt");
+
+    if(!log.is_open()){
+        std::cerr << "[Erro] Nao foi possivel criar o log.txt" << std::endl;
+        return;
+    }
+
+    std::string string;
+    ULA ula;
+
+    log << "Comeco do programa\n";
+    log << "============================================================\n";
+
+    int c = 1;
+
+    while (std::getline(file, string)) {
+        log << "Ciclo " << c << std::endl;
+
+        if(string.length() != 8){
+            log << "[Erro] Numero de bits invalido\n";
+            log << "============================================================\n";
+            break;
+        }
+
+        try{
+            ula.executa(string_to_binary(string));
+            
+            log << "PC = " << int(ula.PC) << std::endl;
+            log << "IR = " << binary_to_string(ula.IR) << std::endl;
+            log << "A = " << binary_to_string(ula.H) << std::endl;
+            log << "B = " << binary_to_string(ula.OPC) << std::endl;
+            log << "SAIDA = " << binary_to_string(ula.saida) << std::endl;
+            log << "SD = " << binary_to_string(ula.sd) << std::endl;
+            log << "N = " << ((ula.N) ? 1 : 0) << std::endl;
+            log << "Z = " << ((ula.Z) ? 1 : 0) << std::endl;
+            log << "VAI_UM = " << ((ula.vai_um) ? 1 : 0) << std::endl;
+
+            log << "============================================================\n";
+        }catch(std::string e){
+            log << "PC = " << int(ula.PC) << std::endl;
+            log << "IR = " << binary_to_string(ula.IR) << std::endl;
+            log << e;
+        }
+        
+        c++;
+    }
+
+    log << "FIM DO PROGRAMA";
+
+    log.close();
+    file.close();
+}
+
+bits_32 FileManager::string_to_binary(std::string string){
+    bits_32 result = 0;
+
+    for(int i = 0; i < 8; i++){
+        result <<= 1;
+
+        if(string[i] == '1'){
+            result |= 1;
+        }
+    }
+
+    return result;
+}
+
+std::string FileManager::binary_to_string(byte b){
+    std::string out;
+
+    for(int i = 7; i >= 0; i--){
+        int bit = (b >> i) & 1;
+
+        if(bit){
+            out += '1';
+            continue;
+        }
+
+        out += '0';
+    }
+
+    return out;
+}
+
+std::string FileManager::binary_to_string(bits_32 b){
+    std::string out;
+
+    for(int i = 31; i >= 0; i--){
+        int bit = (b >> i) & 1;
+
+        if(bit){
+            out += '1';
+            continue;
+        }
+
+        out += '0';
+    }
+
+    return out;
+}
---
+++ Maquina-Mic-1-main/src/include/ula.h
@@ -0,0 +1,27 @@
+#ifndef ULA_H
+#define ULA_H
+
+#include "data_types.h"
+#include <iostream>
+
+class ULA{
+    public:
+        byte IR, MBR, MBRU; //Registradores de 8 bits
+        bits_32 H, OPC, TOS, CPP, LV, SP, PC, MDR, MAR; // Registradores de 32 bits
+
+        bits_32 saida, sd; // Saída da ULA em 32 bits
+        bool vai_um, N, Z; // Saída da ULA em 8 bits;
+
+        ULA();
+
+        void executa(byte inst); // Executa uma instrução
+        void somador_completo(bits_32 a, bits_32 b); // Adiciona dois bits
+        bits_32 Set_BarramentoB(bits_21 entrada); // Seleciona o registrador para o barramento B
+        bits_32 Set_BarramentoC(bits_21 entrada); //Seleciona os registradores para o barramento C
+
+    private:
+        void print_binary(byte b); // Apenas pra debug
+        void print_binary(bits_32 b); // Apenas pra debug
+};
+
+#endif
